5de399f1268197c28b88d7f1520f0ee79d786456
Emanuele Minotto <minottoemanuele@gmail.com>
2015-01-29 23:35:45 +0100

    Tests (fixes #2)

--- Tests/DependencyInjection/TwigCacheExtensionTest.php
+++ Tests/DependencyInjection/TwigCacheExtensionTest.php
@@ -2,6 +2,7 @@
 
 namespace EmanueleMinotto\TwigCacheBundle\Tests\DependencyInjection;
 
+use EmanueleMinotto\TwigCacheBundle\Tests\AppKernel;
 use PHPUnit_Framework_TestCase;
 
 /**
@@ -9,8 +10,45 @@ use PHPUnit_Framework_TestCase;
  */
 class TwigCacheExtensionTest extends PHPUnit_Framework_TestCase
 {
-    public function testPlaceholder()
+    /**
+     * @var \Symfony\Component\HttpKernel\Kernel
+     */
+    private $kernel;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
     {
-        $this->assertTrue(true);
+        $this->kernel = new AppKernel('TwigCacheExtensionTest', true);
+        $this->kernel->boot();
+    }
+
+    /**
+     * @covers ::load
+     */
+    public function testService()
+    {
+        $container = $this->kernel->getContainer();
+
+        $this->assertTrue($container->has('twig_cache.extension'));
+        $this->assertInstanceOf('Twig_Extension', $container->get('twig_cache.extension'));
+
+        $this->assertTrue($container->has('twig_cache.service'));
+        $this->assertInstanceOf('Doctrine\\Common\\Cache\\Cache', $container->get('twig_cache.service'));
+    }
+    /**
+     * @covers ::load
+     */
+    public function testParameter()
+    {
+        $container = $this->kernel->getContainer();
+
+        $this->assertTrue($container->hasParameter('twig_cache.adapter.class'));
+        $this->assertTrue($container->hasParameter('twig_cache.extension.class'));
+        $this->assertTrue($container->hasParameter('twig_cache.strategy.class'));
+        $this->assertTrue($container->hasParameter('twig_cache.strategy.generational.class'));
+        $this->assertTrue($container->hasParameter('twig_cache.strategy.lifetime.class'));
+        $this->assertTrue($container->hasParameter('twig_cache.strategy.spl_object_hash_key_generator.class'));
     }
 }
