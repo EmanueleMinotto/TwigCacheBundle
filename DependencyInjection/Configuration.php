1b9a670c44ff11077279b9a6d18885481389ce1e
Michał Młodziński <mlodzinski+github@gmail.com>
2020-06-09 06:09:45 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,8 +19,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('twig_cache');
+        $treeBuilder = new TreeBuilder('twig_cache', 'array');
+        
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('twig_cache', 'array');
+        }
 
         $rootNode
             ->children()
